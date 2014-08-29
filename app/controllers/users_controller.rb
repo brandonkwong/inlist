5032f07aa467d3ef1d62599fef2f05e1ee6d8164
codingchik <anabel.cuevas4@gmail.com>
2014-08-29 10:22:14 -0700

    created categories model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,10 +18,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = user.id.to_s
+      session[:user_id] = @user.id.to_s
       redirect_to root_path
     else
-      # render :new
+      render 'new'
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140829014518) do
+ActiveRecord::Schema.define(version: 20140829165811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "categories", ["user_id"], name: "index_categories_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "username"
