850960d5110f37bb841c2a3aa3896ac55847bca5
brandonkwong <brandonjk@gmail.com>
2014-08-28 18:46:55 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Inlist::Application.config.secret_key_base = 'a752a83d1a7f4af3602b90de4ee0af19885af7b361eeaa119e4c96f217db635c91c26730b0e20dce1cc7400ba1fbbe9d1c7803127fc6a2fdf8898a0fbaea18d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Inlist::Application.config.session_store :cookie_store, key: '_inlist_session'
