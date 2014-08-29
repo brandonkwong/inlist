fccaa18bbea806a496eb6730d1fb0dd5e78b1ead
codingchik <anabel.cuevas4@gmail.com>
2014-08-29 12:23:38 -0700

    added testing to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
+
+  validates_presence_of :name
+
+  validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :password, confirmation: true, presence: true
   has_secure_password
+
+  has_many :items
+  has_many :categories
 end
