57977c637dda1c7b0c6399fdee5c4fb483b4fb8e
brandonkwong <brandonjk@gmail.com>
2014-09-04 23:06:05 -0700

    Updated filter for image rows

--- app/assets/javascripts/app/filters.js
+++ app/assets/javascripts/app/filters.js
@@ -1,8 +1,8 @@
-inlistApp.filter('array', function() {
+inlistApp.filter('row', function() {
   return function(arrayLength) {
     arrayLength = Math.ceil(arrayLength);
-    var arr = new Array(arrayLength), i = 0;
-    for (; i < arrayLength; i++) {
+    var arr = new Array(arrayLength);
+    for (var i = 0; i < arrayLength; i++) {
       arr[i] = i;
     }
     return arr;
