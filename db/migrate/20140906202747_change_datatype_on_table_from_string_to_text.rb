8a54617682d7c47a8039c701678285502f670664
codingchik <anabel.cuevas4@gmail.com>
2014-09-06 16:01:46 -0700

    changed datatype for items images

--- db/migrate/20140906202747_change_datatype_on_table_from_string_to_text.rb
+++ db/migrate/20140906202747_change_datatype_on_table_from_string_to_text.rb
@@ -1,17 +1,8 @@
 class ChangeDatatypeOnTableFromStringToText < ActiveRecord::Migration
   def up
-<<<<<<< HEAD
     change_column :items, :image, :text, :limit => nil
   end
-
   def down
     change_column :items, :image, :string
-=======
-    change_column :items, :url, :text, :limit => nil
-  end
-
-  def down
-    change_column :items, :url, :string
->>>>>>> master
   end
 end
