cdf8c5401dff94772d1c164d198fabfa1c891337
codingchik <anabel.cuevas4@gmail.com>
2014-09-07 22:59:47 -0700

    use unicorn via procfile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  serialization_scope :view_context
+  #serialization_scope :view_context
 
   private
 
